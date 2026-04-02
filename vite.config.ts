b90659825953ac57e636f952f4f40c5b9d15bc59
lansingalong <lansingc418@gmail.com>
2026-04-02 10:33:30 -0400

    Update base URL to /gc-haven/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/guidingcare-cwf/',
+  base: '/gc-haven/',
   plugins: [react()],
   resolve: {
     alias: {
